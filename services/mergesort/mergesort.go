a0a497f2e59e3e12e15714e4587e0cf2866c058c
prusya <agat.nk@gmail.com>
2023-01-07 19:02:02 -0700

    Update mergesort.go

--- services/mergesort/mergesort.go
+++ services/mergesort/mergesort.go
@@ -20,57 +20,16 @@ func (s *Service) DistributedSort(arr []int) ([]int, error) {
 		return arr, nil
 	}
 
-	var left, right []int
-	var err error
-	if config.MergesortUrl != "" {
-		arrLeft := fmt.Sprintf("%#v", arr[:len(arr)/2])
-		arrLeft = strings.TrimPrefix(arrLeft, "[]int{")
-		arrLeft = strings.TrimSuffix(arrLeft, "}")
-		arrLeft = strings.ReplaceAll(arrLeft, " ", "")
-		urlLeft := fmt.Sprintf("%s?arr=%s", config.MergesortUrl, arrLeft)
-		respLeft, err := http.Get(urlLeft)
-		if err != nil {
-			fmt.Println(err)
-			return nil, err
-		}
-		defer respLeft.Body.Close()
-		err = json.NewDecoder(respLeft.Body).Decode(&left)
-		if err != nil {
-			fmt.Println(err)
-			return nil, err
-		}
-
-		arrRight := fmt.Sprintf("%#v", arr[len(arr)/2:])
-		arrRight = strings.TrimPrefix(arrRight, "[]int{")
-		arrRight = strings.TrimSuffix(arrRight, "}")
-		arrRight = strings.ReplaceAll(arrRight, " ", "")
-		urlRight := fmt.Sprintf("%s?arr=%s", config.MergesortUrl, arrRight)
-		respRight, err := http.Get(urlRight)
-		if err != nil {
-			fmt.Println(err)
-			return nil, err
-		}
-		defer respRight.Body.Close()
-		err = json.NewDecoder(respRight.Body).Decode(&right)
-		if err != nil {
-			fmt.Println(err)
-			return nil, err
-		}
-	} else {
-		left, err = s.DistributedSort(arr[:len(arr)/2])
-		if err != nil {
-			fmt.Println(err)
-			return nil, err
-		}
-		right, err = s.DistributedSort(arr[len(arr)/2:])
-		if err != nil {
-			fmt.Println(err)
-			return nil, err
-		}
+	left, err := s.distributedSort(arr[:len(arr)/2])
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
 	}
-
-	// fmt.Println("left", left)
-	// fmt.Println("right", right)
+	right, err := s.distributedSort(arr[len(arr)/2:])
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
 
 	return merge(left, right), nil
 }
@@ -106,4 +65,36 @@ func (s *Service) JobQueueSort(arr []int) (string, error) {
 
 func (s *Service) JobQueueSortResults(jobID string) ([]int, error) {
 	return []int{}, nil
+}
+
+func (s *Service) distributedSort(arr []int) ([]int, error) {
+	var out []int
+	var err error
+
+	if config.MergesortUrl != "" {
+		payload := fmt.Sprintf("%#v", arr)
+		payload = strings.TrimPrefix(payload, "[]int{")
+		payload = strings.TrimSuffix(payload, "}")
+		payload = strings.ReplaceAll(payload, " ", "")
+		urlLeft := fmt.Sprintf("%s?arr=%s", config.MergesortUrl, payload)
+		respLeft, err := http.Get(urlLeft)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		defer respLeft.Body.Close()
+		err = json.NewDecoder(respLeft.Body).Decode(&out)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+	} else {
+		out, err = s.DistributedSort(arr)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+	}
+
+	return out, nil
 }
